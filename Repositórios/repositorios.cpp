da0b796ef447f3ab00d8057195e778c79b607466
Bruno Vinícius Alves Nogueira <100003070+vrunobinicius@users.noreply.github.com>
2024-04-30 20:45:02 -0300

    Repositórios
    CONCLUIDO DESAFIO REPOSITÓRIO

--- Repositórios/repositorios.cpp
+++ Repositórios/repositorios.cpp
@@ -3,23 +3,36 @@
 
 using namespace std;
 
-using namespace std;
+int main(int argc, char const *argv[]){
 
-int main(int argc, char const *argv[])
-{
+    int C, N, Pc, Vc, Pn, Vn;
+    map<int, int> programas, respostas;
 
-    map<char, int> mp;
-
-    mp.insert({'a', 10});
-    mp.insert({'b', 20});
-    mp.insert({'c', 30});
-    mp['d'] = 40;
-
-    for (auto a : mp)
+    cin >> C >> N;
+    for (int i = 0; i < C; i++)
     {
-        cout << a.first << " " << a.second << "\n";
+        cin >> Pc >> Vc;
+        programas.insert({Pc, Vc});
     }
-    
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> Pn >> Vn;
+        if (programas.find(Pn) == programas.end())
+        {
+            programas[Pn] = Vn;
+            respostas[Pn] = Vn;
+        } 
+        if (programas[Pn] < Vn) {
+            programas[Pn] = Vn;
+            respostas[Pn] = Vn;
+        }
+    }
+
+    for (auto i = respostas.begin(); i != respostas.end(); i++)
+    {
+        cout << i->first << ' ' << i->second << '\n';
+    }
 
     return 0;
 }
